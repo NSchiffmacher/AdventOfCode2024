102cbb65d6bb6db5a98af51607ad63f50e64e906
NSchiffmacher <nathan.schiffmacher@gmail.com>
2024-12-01 10:59:52 +0100

    Day 1 🎅

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::io::{self, Write};
 
@@ -6,6 +7,8 @@ use itertools::Itertools;
 
 pub struct Solution {
     lines: Vec<String>,
+    list_a: Vec<i32>,
+    list_b: Vec<i32>,
 }
 
 impl Solution {
@@ -15,17 +18,55 @@ impl Solution {
             lines.push(line.to_string());
         }
 
+        let mut list_a: Vec<i32> = Vec::new();
+        let mut list_b: Vec<i32> = Vec::new();
+
+        for line in &lines {
+            let (a, b) = line.split_once("   ").unwrap();
+            let a = a.parse().unwrap();
+            let b = b.parse().unwrap();
+
+            list_a.push(a);
+            list_b.push(b);
+        }
+
+        list_a.sort_unstable();
+        list_b.sort_unstable();
+
         Self {
             lines,
+            list_a,
+            list_b,
         }
     }
 
-    fn part1(&mut self) {
-
+    fn part1(&mut self) -> i32 {
+        self.list_a
+            .iter()
+            .zip(&self.list_b)
+            .map(|(a, b)| (a - b).abs())
+            .sum()
     }
 
-    fn part2(&mut self) {
+    fn make_occurences_map(&self, list: &[i32]) -> HashMap<i32, i32> {
+        list
+            .iter()
+            .copied()
+            .fold(HashMap::new(), |mut map, e| {
+                *map.entry(e).or_insert(0) += 1;
+                map
+            })
+    }
 
+    fn part2(&mut self) -> i32 {
+        let occurences_a = self.make_occurences_map(&self.list_a);
+        let occurences_b = self.make_occurences_map(&self.list_b);
+
+        occurences_a
+            .iter()
+            .fold(0, |acc, (e, count)| {
+                acc + e * count * occurences_b.get(e).unwrap_or(&0)
+            })
     }
 
     pub fn solve(&mut self) {
